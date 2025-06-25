4003bd25c9ed0c6866c1204ddc064b465301fc21
Nimith B <nimithbe@gmail.com>
2025-06-25 16:36:35 +0530

    Add mandatory and extra solutions, ignore code and src folders

--- WEEK1/DesignPatternsExamples/Design Patterns and Principles/FactoryMethodPatternExample/src/Document.java
+++
@@ -1,3 +0,0 @@
-public interface Document {
-    String getType();
-}
--- WEEK1/DesignPatternsExamples/Design Patterns and Principles/FactoryMethodPatternExample/src/PdfDocument.java
+++
@@ -1,5 +0,0 @@
-public class PdfDocument implements Document {
-    public String getType() {
-        return "PdfDocument";
-    }
-}
